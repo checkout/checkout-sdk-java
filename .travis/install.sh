099dd14e53265f34bfc5196d31a607914974402c
Harry Peirse <peirse@gmail.com>
2019-06-28 21:58:00 +0100

    Use different encrypted values for travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,4 +1,4 @@
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-	openssl aes-256-cbc -K $encrypted_175f9c159b8a_key -iv $encrypted_175f9c159b8a_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
+    openssl aes-256-cbc -K $encrypted_017171c4686e_key -iv $encrypted_017171c4686e_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
 fi
