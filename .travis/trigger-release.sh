7b2c977e067ab08cb5bb3df098419616514c94bf
Harry Peirse <peirse@gmail.com>
2019-07-01 21:55:30 +0100

    Use --com version of travis

--- .travis/trigger-release.sh
+++ .travis/trigger-release.sh
@@ -8,9 +8,9 @@ GITHUB_REPOSITORY_NAME=checkout-sdk-java
 # Assumptions
 # - This is called from the root of the project
 # - The travis client is installed: gem install travis
-# - travis login --org has been called to authenticate
+# - travis login --com has been called to authenticate
 
-TRAVIS_PERSONAL_TOKEN=$(travis token)
+TRAVIS_PERSONAL_TOKEN=$(travis token --com)
 
 :${TRAVIS_PERSONAL_TOKEN:?"TRAVIS_PERSONAL_TOKEN needs to be set to access the Travis API to trigger the build"}
 
@@ -32,4 +32,4 @@ curl -s -X POST \
     -H "Travis-API-Version: 3" \
     -H "Authorization: token $TRAVIS_PERSONAL_TOKEN" \
     -d "$body" \
-    https://api.travis-ci.org/repo/$GITHUB_ORGANIZATION%2F$GITHUB_REPOSITORY_NAME/requests
+    https://api.travis-ci.com/repo/$GITHUB_ORGANIZATION%2F$GITHUB_REPOSITORY_NAME/requests
