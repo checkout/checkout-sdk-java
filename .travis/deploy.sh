9f6299841fa889245e580dbdcfe7c81ad5cb825e
Harry Peirse <peirse@gmail.com>
2019-07-04 20:49:10 +0100

    Use gradle plugin instead of shell scripts for releasing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
 then
-	if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
-	then
-		echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
-		./gradlew uploadArchives publishToNexusAndClose
-	fi
+	echo "Promote repository"
+	./gradlew publish closeAndReleaseRepository
 fi
