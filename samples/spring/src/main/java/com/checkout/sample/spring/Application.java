e2017c43932bae4929584fb400bad27f664cda4d
Harry Peirse <peirse@gmail.com>
2019-08-02 22:43:34 +0100

    Fix up spring boot example

--- samples/spring/src/main/java/com/checkout/sample/spring/Application.java
+++ samples/spring/src/main/java/com/checkout/sample/spring/Application.java
@@ -1,13 +1,11 @@
 package com.checkout.sample.spring;
 
-import com.checkout.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
