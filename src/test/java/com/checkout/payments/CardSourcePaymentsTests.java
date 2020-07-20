f3f91f597811bb89329fb90ca7d71b4e8d93d8df
Anton Sytnyk <anton.sytnyk@transferwise.com>
2020-07-20 16:06:22 +0300

    added stored field to support stored card details requirements

--- src/test/java/com/checkout/payments/CardSourcePaymentsTests.java
+++ src/test/java/com/checkout/payments/CardSourcePaymentsTests.java
@@ -29,6 +29,7 @@ public class CardSourcePaymentsTests extends ApiTestFixture {
         Assert.assertTrue(paymentResponse.getPayment().canCapture());
         Assert.assertTrue(paymentResponse.getPayment().canVoid());
         Assert.assertNotNull(paymentResponse.getPayment().getSource());
+        Assert.assertFalse(paymentRequest.getSource().isStored());
     }
 
     @Test
