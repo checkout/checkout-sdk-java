de1c2a7901f30040a0e3d50e70c93f8106d205c6
ioan.ghisoi95 <ionutghisoi95@gmail.com>
2020-08-21 00:40:12 +0100

    Update Docusaurus Config

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,57 +1,57 @@
 module.exports = {
-    title: 'checkout-sdk-java',
-    tagline: 'Checkout.com SDK for Java',
-    url: 'https://stephen-gilbert-cko.github.io',
-    baseUrl: '/checkout-sdk-java/',
-    favicon: 'img/favicon.png',
-    organizationName: 'stephen-gilbert-cko', // Usually your GitHub org/user name.
-    projectName: 'checkout-sdk-java', // Usually your repo name.
-    themeConfig: {
-        navbar: {
-            title: 'checkout-sdk-java',
-            logo: {
-                alt: 'checkout-sdk-java',
-                src: 'img/logo.png',
-            },
-            links: [
-                {
-                    to: 'getting_started',
-                    activeBasePath: 'docs',
-                    label: 'Docs',
-                    position: 'right',
-                },
-                {
-                    href: 'https://github.com/stephen-gilbert-cko/checkout-sdk-java',
-                    label: 'GitHub',
-                    position: 'right',
-                },
-            ],
+  title: "checkout-sdk-java",
+  tagline: "Checkout.com SDK for Java",
+  url: "https://checkout.github.io",
+  baseUrl: "/checkout-sdk-java/",
+  favicon: "img/favicon.png",
+  organizationName: "checkout", // Usually your GitHub org/user name.
+  projectName: "checkout-sdk-java", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "checkout-sdk-java",
+      logo: {
+        alt: "checkout-sdk-java",
+        src: "img/logo.png",
+      },
+      links: [
+        {
+          to: "getting_started",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "right",
         },
-        footer: {
-            style: 'dark',
-            copyright: `© ${new Date().getFullYear()} Checkout.com    `,
+        {
+          href: "https://github.com/stephen-gilbert-cko/checkout-sdk-java",
+          label: "GitHub",
+          position: "right",
         },
-        googleAnalytics: {
-            trackingID: 'UA-165971486-1',
-        },
+      ],
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
-            {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    routeBasePath: '',
-                    // editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/'
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            },
-        ],
+    footer: {
+      style: "dark",
+      copyright: `© ${new Date().getFullYear()} Checkout.com    `,
+    },
+    googleAnalytics: {
+      trackingID: "UA-165971486-1",
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "",
+          // editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/'
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
     ],
-    plugins: [
-        // Basic usage.
-        require.resolve('@docusaurus/plugin-google-analytics'),
-    ],
+  ],
+  plugins: [
+    // Basic usage.
+    require.resolve("@docusaurus/plugin-google-analytics"),
+  ],
 };
