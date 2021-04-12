f4a650bc7c25e9038a0cf9694eb4cc2f2436397a
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2021-04-12 17:21:30 +0100

    Add name fields for the Id Source

--- src/main/java/com/checkout/payments/IdSource.java
+++ src/main/java/com/checkout/payments/IdSource.java
@@ -14,6 +14,8 @@ public class IdSource implements RequestSource {
     public static final String TYPE_NAME = "id";
     private String id;
     private String cvv;
+    private String firstName;
+    private String lastName;
 
     private final String type = TYPE_NAME;
 
@@ -27,5 +29,23 @@ public class IdSource implements RequestSource {
     @Override
     public String getType() {
         return TYPE_NAME;
+    }
+
+     /**
+     * Set the first name of a payout card destination object.
+     *
+     * @param firstName the first name of the destination card holder
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Set the last name of a payout card destination object.
+     *
+     * @param lastName the last name of the destination card holder
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
