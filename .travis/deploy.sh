ff087bdd4f2fcc4b8b0e91bb0942964487002bd7
Raul Estrela <rsaestrela@gmail.com>
2021-08-20 15:46:29 +0200

    Migrate to GitHub Actions

--- .travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
-then
-	echo "Promote repository"
-	./gradlew publishToSonatype closeAndReleaseRepository
-fi
