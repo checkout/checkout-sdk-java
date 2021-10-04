f1c33fd1de434f21548a29075549e718565e9174
Raul Estrela <rsaestrela@gmail.com>
2021-10-04 17:20:37 +0200

    OAuth (CS2) - Add support for default authorization URIs
    This commit adds support for default `SANDBOX` and `PRODUCTION`
    configuration based OAuth authorization URIs.

--- src/main/java/com/checkout/Environment.java
+++ src/main/java/com/checkout/Environment.java
@@ -2,15 +2,19 @@ package com.checkout;
 
 public enum Environment {
 
-    SANDBOX("https://api.sandbox.checkout.com/", "https://files.sandbox.checkout.com/"),
-    PRODUCTION("https://api.checkout.com/", "https://files.checkout.com/");
+    SANDBOX("https://api.sandbox.checkout.com/", "https://files.sandbox.checkout.com/", "https://access.sandbox.checkout.com/connect/token"),
+    PRODUCTION("https://api.checkout.com/", "https://files.checkout.com/", "https://access.checkout.com/connect/token");
 
     private final String uri;
     private final String filesApiURI;
+    private final String oauthAuthorizeURI;
 
-    Environment(final String uri, final String filesApiURI) {
+    Environment(final String uri,
+                final String filesApiURI,
+                final String oauthAuthorizeURI) {
         this.uri = uri;
         this.filesApiURI = filesApiURI;
+        this.oauthAuthorizeURI = oauthAuthorizeURI;
     }
 
     public String getUri() {
@@ -19,6 +23,10 @@ public enum Environment {
 
     public String getFilesApiURI() {
         return filesApiURI;
+    }
+
+    public String getOauthAuthorizeURI() {
+        return oauthAuthorizeURI;
     }
 
     /**
