6800e4730a2415a73dc0d30737e38d5c648abe4b
Raul Estrela <rsaestrela@gmail.com>
2021-10-27 11:58:04 +0200

    Improve Payments CS2 payouts request destination
    This commit adds specific `PaymentBankAccount` and `RequestId` payout destination
    classes, instead of having a generic `PaymentDestination` class.
    
    It also changes the `LICENSE.md` reference in the `README.md` to a relative location.

--- src/main/java/com/checkout/payments/four/payout/PayoutRequest.java
+++ src/main/java/com/checkout/payments/four/payout/PayoutRequest.java
@@ -2,6 +2,7 @@ package com.checkout.payments.four.payout;
 
 import com.checkout.common.Currency;
 import com.checkout.payments.four.BillingDescriptor;
+import com.checkout.payments.four.payout.destination.PaymentDestination;
 import com.checkout.payments.four.payout.source.PayoutRequestSource;
 import com.checkout.payments.four.sender.RequestSender;
 import com.google.gson.annotations.SerializedName;
