79772d3f51548175c8493065bd1b09100ab2b84e
sean@policarp.io <sean@policarp.io>
2021-11-19 15:01:35 +1100

    add `name` and `city` fields
    This is to match `com.checkout.payments.BillingDescriptor`

--- src/main/java/com/checkout/payments/four/BillingDescriptor.java
+++ src/main/java/com/checkout/payments/four/BillingDescriptor.java
@@ -1,14 +1,16 @@
 package com.checkout.payments.four;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public final class BillingDescriptor {
-
+    private String name;
+    private String city;
     private String reference;
-
 }
