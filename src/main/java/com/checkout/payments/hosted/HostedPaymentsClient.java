2cadb2d0951474a00a47f33fb63f0eb134878174
Armando Ibarra <aibarrakerzon@gmail.com>
2021-12-29 17:39:51 -0600

    Add Get Hosted Payments Page details endpoint to HostedPaymentsClient.
    Add missing property id to HostedPaymentResponse

--- src/main/java/com/checkout/payments/hosted/HostedPaymentsClient.java
+++ src/main/java/com/checkout/payments/hosted/HostedPaymentsClient.java
@@ -3,5 +3,9 @@ package com.checkout.payments.hosted;
 import java.util.concurrent.CompletableFuture;
 
 public interface HostedPaymentsClient {
+
     CompletableFuture<HostedPaymentResponse> createAsync(HostedPaymentRequest hostedPaymentRequest);
+
+    CompletableFuture<HostedPaymentDetailsResponse> get(String hostedPaymentId);
+
 }
--- src/test/java/com/checkout/payments/hosted/HostedPaymentsClientImplTest.java
+++ src/test/java/com/checkout/payments/hosted/HostedPaymentsClientImplTest.java
@@ -5,15 +5,12 @@ import com.checkout.CheckoutConfiguration;
 import com.checkout.SdkAuthorization;
 import com.checkout.SdkAuthorizationType;
 import com.checkout.SdkCredentials;
-import com.checkout.TestHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -21,13 +18,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class HostedPaymentsClientImplTest {
 
-    private static final String HOSTED_PAYMENTS = "/hosted-payments";
-    private static final String REFERENCE = "ORD-1234";
+    private HostedPaymentsClient client;
 
     @Mock
     private ApiClient apiClient;
@@ -41,32 +39,38 @@ class HostedPaymentsClientImplTest {
     @Mock
     private SdkAuthorization authorization;
 
-    @Mock
-    private HostedPaymentResponse response;
-
-    @Mock
-    private CompletableFuture<HostedPaymentResponse> responseAsync;
-
     @BeforeEach
     void setUp() {
         when(sdkCredentials.getAuthorization(SdkAuthorizationType.SECRET_KEY)).thenReturn(authorization);
         when(configuration.getSdkCredentials()).thenReturn(sdkCredentials);
-        responseAsync = CompletableFuture.completedFuture(response);
-        when(response.getReference()).thenReturn(REFERENCE);
-        when(response.getLinks()).thenReturn(new HashMap<>());
+        client = new HostedPaymentsClientImpl(apiClient, configuration);
     }
 
     @Test
     void shouldCreateHostedPayments() throws ExecutionException, InterruptedException {
-        final HostedPaymentRequest request = TestHelper.createHostedPaymentRequest(REFERENCE);
+
+        final HostedPaymentRequest request = mock(HostedPaymentRequest.class);
+        final HostedPaymentResponse response = mock(HostedPaymentResponse.class);
+
+        when(apiClient.postAsync(eq("hosted-payments"), eq(authorization), eq(HostedPaymentResponse.class),
+                eq(request), isNull()))
+                .thenReturn(CompletableFuture.completedFuture(response));
+
+        final CompletableFuture<HostedPaymentResponse> future = client.createAsync(request);
+
+        assertNotNull(future.get());
+        assertEquals(response, future.get());
+    }
+
+    @Test
+    void shouldGetHostedPayment() throws ExecutionException, InterruptedException {
+
         final HostedPaymentsClient client = new HostedPaymentsClientImpl(apiClient, configuration);
-        Mockito.doReturn(responseAsync)
-                .when(apiClient).postAsync(eq(HOSTED_PAYMENTS), any(SdkAuthorization.class),
-                eq(HostedPaymentResponse.class), any(HostedPaymentRequest.class), any());
-        final HostedPaymentResponse hostedPaymentResponse = client.createAsync(request).get();
-        assertNotNull(hostedPaymentResponse);
-        assertEquals(request.getReference(), hostedPaymentResponse.getReference());
-        assertNotNull(hostedPaymentResponse.getLinks());
+        when(apiClient.getAsync(eq("hosted-payments/hosted_id"), any(SdkAuthorization.class), eq(HostedPaymentDetailsResponse.class)))
+                .thenReturn(CompletableFuture.completedFuture(new HostedPaymentDetailsResponse()));
+
+        final HostedPaymentDetailsResponse response = client.get("hosted_id").get();
+        assertNotNull(response);
     }
 
 }
