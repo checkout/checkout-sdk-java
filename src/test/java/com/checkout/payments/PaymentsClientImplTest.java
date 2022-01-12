checkout/checkout-sdk-java

commit 9ac70a13f4b5573591339cb37ded49827bc8ee58

Added new values PaymentSourceType

src/main/java/com/checkout/common/PaymentSourceType.java
@@ -39,6 +39,23 @@ public enum PaymentSourceType {
     @SerializedName("id")
     SEPA,
     @SerializedName("sofort")
-    SOFORT;
-
+    SOFORT,
+    @SerializedName("qpay")
+    QPAY,
+    @SerializedName("alipay")
+    ALIPAY,
+    @SerializedName("paypal")
+    PAYPAL,
+    @SerializedName("multibanco")
+    MULTIBANCO,
+    @SerializedName("eps")
+    EPS,
+    @SerializedName("poli")
+    POLI,
+    @SerializedName("p24")
+    PRZELEWY24,
+    @SerializedName("benefitpay")
+    BENEFITPAY,
+    @SerializedName("bancontact")
+    BANCONTACT;
 }

src/test/java/com/checkout/payments/PaymentsClientImplTest.java
@@ -1,12 +1,11 @@
 package com.checkout.payments;
 
-import com.checkout.ApiClient;
-import com.checkout.CheckoutConfiguration;
-import com.checkout.SdkAuthorization;
-import com.checkout.SdkAuthorizationType;
-import com.checkout.SdkCredentials;
+import com.checkout.*;
+import com.checkout.common.Currency;
+import com.checkout.common.PaymentSourceType;
 import com.checkout.payments.request.PaymentRequest;
 import com.checkout.payments.request.PayoutRequest;
+import com.checkout.payments.request.source.AbstractRequestSource;
 import com.checkout.payments.response.GetPaymentResponse;
 import com.checkout.payments.response.PaymentResponse;
 import com.google.gson.reflect.TypeToken;
@@ -24,9 +23,7 @@ import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -77,6 +74,41 @@ class PaymentsClientImplTest {
         assertNotNull(future.get());
         assertEquals(response, future.get());
 
+    }
+
+    @Test
+    void shouldRequestPayment_customSource() throws ExecutionException, InterruptedException {
+
+        CustomSource customSource = new CustomSource(10, Currency.USD);
+
+        final PaymentRequest request = mock(PaymentRequest.class);
+        request.setSource(customSource);
+
+        final PaymentResponse response = PaymentRequest.builder().source(customSource).build()
+
+        when(apiClient.postAsync(eq(PAYMENTS_PATH), any(SdkAuthorization.class), eq(PaymentResponse.class), eq(request), isNull()))
+                .thenReturn(CompletableFuture.completedFuture(response));
+
+        final CompletableFuture<PaymentResponse> future = paymentsClient.requestPayment(request);
+
+        assertNotNull(future.get());
+        assertEquals(response, future.get());
+
+    }
+
+    private class CustomSource extends AbstractRequestSource {
+
+        private long amount;
+
+        private Currency currency;
+
+        protected CustomSource(final long amount,
+                               final Currency currency) {
+
+            super(PaymentSourceType.ALIPAY);
+            this.amount = amount;
+            this.currency = currency;
+        }
     }
 
     @Test

commit 252ce68cb2e8dae12529d3804f6a0d958cffaef0

Fix compilation issue introduced in 9ac70a1

src/main/java/com/checkout/common/PaymentSourceType.java
@@ -57,5 +57,5 @@ public enum PaymentSourceType {
     @SerializedName("benefitpay")
     BENEFITPAY,
     @SerializedName("bancontact")
-    BANCONTACT;
+    BANCONTACT
 }

src/test/java/com/checkout/payments/PaymentsClientImplTest.java
@@ -1,6 +1,10 @@
 package com.checkout.payments;
 
-import com.checkout.*;
+import com.checkout.ApiClient;
+import com.checkout.CheckoutConfiguration;
+import com.checkout.SdkAuthorization;
+import com.checkout.SdkAuthorizationType;
+import com.checkout.SdkCredentials;
 import com.checkout.common.Currency;
 import com.checkout.common.PaymentSourceType;
 import com.checkout.payments.request.PaymentRequest;
@@ -9,6 +13,8 @@ import com.checkout.payments.request.source.AbstractRequestSource;
 import com.checkout.payments.response.GetPaymentResponse;
 import com.checkout.payments.response.PaymentResponse;
 import com.google.gson.reflect.TypeToken;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,7 +29,9 @@ import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -79,12 +87,10 @@ class PaymentsClientImplTest {
     @Test
     void shouldRequestPayment_customSource() throws ExecutionException, InterruptedException {
 
-        CustomSource customSource = new CustomSource(10, Currency.USD);
+        final CustomSource customSource = new CustomSource(10, Currency.USD);
 
-        final PaymentRequest request = mock(PaymentRequest.class);
-        request.setSource(customSource);
-
-        final PaymentResponse response = PaymentRequest.builder().source(customSource).build()
+        final PaymentRequest request = PaymentRequest.builder().source(customSource).build();
+        final PaymentResponse response = mock(PaymentResponse.class);
 
         when(apiClient.postAsync(eq(PAYMENTS_PATH), any(SdkAuthorization.class), eq(PaymentResponse.class), eq(request), isNull()))
                 .thenReturn(CompletableFuture.completedFuture(response));
@@ -96,10 +102,11 @@ class PaymentsClientImplTest {
 
     }
 
-    private class CustomSource extends AbstractRequestSource {
+    @Getter
+    @Setter
+    private static class CustomSource extends AbstractRequestSource {
 
         private long amount;
-
         private Currency currency;
 
         protected CustomSource(final long amount,