cef32d0b00609c6f84a6a7642d477d9ea2bf2e21
Armando Ibarra <aibarrakerzon@gmail.com>
2022-05-03 22:30:17 -0500

    Deprecate unnecessary APM clients.
    These clients are only supported on Sandbox, right now there is no need to expose these functionalities through the SDK

--- src/main/java/com/checkout/apm/pagofacil/PagoFacilClient.java
+++ src/main/java/com/checkout/apm/pagofacil/PagoFacilClient.java
@@ -2,6 +2,10 @@ package com.checkout.apm.pagofacil;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * @deprecated Won't be supported anymore on further versions
+ */
+@Deprecated
 public interface PagoFacilClient {
 
     CompletableFuture<Void> succeed(String paymentId);
