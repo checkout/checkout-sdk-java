bea217ef49296ba0c148b4fc95f5949c006c3283
Armando Ibarra <aibarrakerzon@gmail.com>
2022-05-03 22:30:17 -0500

    Deprecate unnecessary APM clients.
    These clients are only supported on Sandbox, right now there is no need to expose these functionalities through the SDK

--- src/main/java/com/checkout/apm/baloto/BalotoClient.java
+++ src/main/java/com/checkout/apm/baloto/BalotoClient.java
@@ -2,6 +2,10 @@ package com.checkout.apm.baloto;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * @deprecated Won't be supported anymore on further versions
+ */
+@Deprecated
 public interface BalotoClient {
 
     CompletableFuture<Void> succeed(String id);
