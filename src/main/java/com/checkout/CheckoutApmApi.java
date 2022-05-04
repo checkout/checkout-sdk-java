0cbe511becea7eec5a9c2d158056de0cb68a2ed6
Armando Ibarra <aibarrakerzon@gmail.com>
2022-05-03 22:30:17 -0500

    Deprecate unnecessary APM clients.
    These clients are only supported on Sandbox, right now there is no need to expose these functionalities through the SDK

--- src/main/java/com/checkout/CheckoutApmApi.java
+++ src/main/java/com/checkout/CheckoutApmApi.java
@@ -11,18 +11,38 @@ import com.checkout.apm.sepa.SepaClient;
 
 public interface CheckoutApmApi {
 
+    /**
+     * @deprecated Won't be supported anymore on further versions
+     */
+    @Deprecated
     BalotoClient balotoClient();
 
+    /**
+     * @deprecated Won't be supported anymore on further versions
+     */
+    @Deprecated
     FawryClient fawryClient();
 
     IdealClient idealClient();
 
     KlarnaClient klarnaClient();
 
+    /**
+     * @deprecated Won't be supported anymore on further versions
+     */
+    @Deprecated
     OxxoClient oxxoClient();
 
+    /**
+     * @deprecated Won't be supported anymore on further versions
+     */
+    @Deprecated
     PagoFacilClient pagoFacilClient();
 
+    /**
+     * @deprecated Won't be supported anymore on further versions
+     */
+    @Deprecated
     RapiPagoClient rapiPagoClient();
 
     SepaClient sepaClient();
--- src/main/java/com/checkout/apm/fawry/FawryClient.java
+++ src/main/java/com/checkout/apm/fawry/FawryClient.java
@@ -2,6 +2,10 @@ package com.checkout.apm.fawry;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * @deprecated  Won't be supported anymore on further versions
+ */
+@Deprecated
 public interface FawryClient {
 
     CompletableFuture<Void> approve(String reference);
