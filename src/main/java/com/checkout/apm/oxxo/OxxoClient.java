bd4abfe615cac2449f1f9fa6ab92c5c32fe7ca86
Armando Ibarra <aibarrakerzon@gmail.com>
2022-05-03 22:30:17 -0500

    Deprecate unnecessary APM clients.
    These clients are only supported on Sandbox, right now there is no need to expose these functionalities through the SDK

--- src/main/java/com/checkout/apm/oxxo/OxxoClient.java
+++ src/main/java/com/checkout/apm/oxxo/OxxoClient.java
@@ -2,6 +2,10 @@ package com.checkout.apm.oxxo;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * @deprecated Won't be supported anymore on further versions
+ */
+@Deprecated
 public interface OxxoClient {
 
     CompletableFuture<Void> succeed(String paymentId);
