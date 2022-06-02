f022227e6e9a58301f88716a158a2fd1f884c4bd
Armando Ibarra <aibarrakerzon@gmail.com>
2022-06-01 20:51:17 -0500

    Add cartes bancaries properties to ThreeDsRequest

--- src/main/java/com/checkout/common/four/ThreeDSFlowType.java
+++ src/main/java/com/checkout/common/four/ThreeDSFlowType.java
@@ -1,4 +1,4 @@
-package com.checkout.sessions;
+package com.checkout.common.four;
 
 import com.google.gson.annotations.SerializedName;
 
--- src/main/java/com/checkout/payments/ThreeDSRequest.java
+++ src/main/java/com/checkout/payments/ThreeDSRequest.java
@@ -2,11 +2,14 @@ package com.checkout.payments;
 
 import com.checkout.common.ChallengeIndicator;
 import com.checkout.common.Exemption;
+import com.checkout.common.four.ThreeDSFlowType;
 import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Data
 @Builder
@@ -31,6 +34,30 @@ public final class ThreeDSRequest {
 
     @SerializedName("challenge_indicator")
     private ChallengeIndicator challengeIndicator;
+
+    // Only available in Four
+
+    private String status;
+
+    @SerializedName("authentication_date")
+    private Instant authenticationDate;
+
+    @SerializedName("authentication_amount")
+    private Long authenticationAmount;
+
+    @SerializedName("flow_type")
+    private ThreeDSFlowType flowType;
+
+    @SerializedName("status_reason_code")
+    private String statusReasonCode;
+
+    @SerializedName("challenge_cancel_reason")
+    private String challengeCancelReason;
+
+    private String score;
+
+    @SerializedName("cryptogram_algorithm")
+    private String cryptogramAlgorithm;
 
 }
 
