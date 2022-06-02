ce956dd693f081282d7d567eab5b4ae987dcf571
Armando Ibarra <aibarrakerzon@gmail.com>
2022-06-01 20:51:17 -0500

    Add cartes bancaries properties to ThreeDsRequest

--- src/main/java/com/checkout/payments/ProcessingSettings.java
+++ src/main/java/com/checkout/payments/ProcessingSettings.java
@@ -20,4 +20,19 @@ public final class ProcessingSettings {
     @SerializedName("shipping_amount")
     private Long shippingAmount;
 
+    @SerializedName("preferred_scheme")
+    private PreferredSchema preferredScheme;
+
+    @SerializedName("product_type")
+    private ProductType productType;
+
+    @SerializedName("open_id")
+    private String openId;
+
+    @SerializedName("original_order_amount")
+    private Long originalOrderAmount;
+
+    @SerializedName("receipt_id")
+    private String receiptId;
+
 }
