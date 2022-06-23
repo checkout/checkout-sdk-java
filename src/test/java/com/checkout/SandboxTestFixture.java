e819bb0e5db60ecba121ea95a083577fdde835bd
Armando Ibarra <aibarrakerzon@gmail.com>
2022-06-22 21:40:36 -0500

    Added missing Processing properties

--- src/test/java/com/checkout/SandboxTestFixture.java
+++ src/test/java/com/checkout/SandboxTestFixture.java
@@ -1,8 +1,20 @@
 package com.checkout;
 
+import lombok.extern.slf4j.Slf4j;
+import org.hamcrest.Matcher;
+import org.opentest4j.AssertionFailedError;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import static org.junit.Assert.fail;
+
+@Slf4j
 public class SandboxTestFixture {
 
     public static final CheckoutConfiguration CONFIGURATION = loadConfiguration();
+    private static final int TRY_MAX_ATTEMPTS = 10;
     private CheckoutApi api;
 
     public SandboxTestFixture() {
@@ -27,5 +39,45 @@ public class SandboxTestFixture {
 
     public CheckoutApi getApi() {
         return api;
+    }
+
+    protected <T> T blocking(final Supplier<CompletableFuture<T>> supplier) {
+        int attempts = 1;
+        while (attempts <= TRY_MAX_ATTEMPTS) {
+            try {
+                return supplier.get().get();
+            } catch (final Throwable e) {
+                log.warn("Request failed with error '{}' - retry {}/{}", e.getMessage(), attempts, TRY_MAX_ATTEMPTS);
+            }
+            attempts++;
+            nap();
+        }
+        throw new AssertionFailedError("Max attempts reached!");
+    }
+
+    protected <T> T blocking(final Supplier<CompletableFuture<T>> supplier, final Matcher<T> matcher) {
+        int attempts = 1;
+        while (attempts <= TRY_MAX_ATTEMPTS) {
+            try {
+                final CompletableFuture<T> t = supplier.get();
+                if (!matcher.matches(t.get())) {
+                    throw new AssertionFailedError(matcher.toString());
+                }
+                return t.get();
+            } catch (final Throwable e) {
+                log.warn("Request/Matcher failed with error '{}' - retry {}/{}", e.getMessage(), attempts, TRY_MAX_ATTEMPTS);
+            }
+            attempts++;
+            nap();
+        }
+        throw new AssertionFailedError("Max attempts reached!");
+    }
+
+    private void nap() {
+        try {
+            TimeUnit.SECONDS.sleep(3L);
+        } catch (final InterruptedException ignore) {
+            fail();
+        }
     }
 }
--- src/test/java/com/checkout/payments/CardDestinationPaymentTests.java
+++ src/test/java/com/checkout/payments/CardDestinationPaymentTests.java
@@ -7,25 +7,34 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CardDestinationPaymentTests extends SandboxTestFixture {
-	@Test
-	public void can_perform_card_payout() throws Exception {
-		PaymentRequest<CardSource> paymentRequest = TestHelper.createCardPayoutRequest();
-		PaymentResponse paymentResponse = getApi().paymentsClient().requestAsync(paymentRequest).get();
+    @Test
+    public void can_perform_card_payout() throws Exception {
+        PaymentRequest<CardSource> paymentRequest = TestHelper.createCardPayoutRequest();
+        PaymentResponse paymentResponse = getApi().paymentsClient().requestAsync(paymentRequest).get();
 
-		Assert.assertNotNull(paymentResponse.getPayment());
-		Assert.assertTrue(paymentResponse.getPayment().isApproved());
-		Assert.assertFalse(CheckoutUtils.isNullOrEmpty(paymentResponse.getPayment().getId()));
-		Assert.assertFalse(CheckoutUtils.isNullOrEmpty(paymentResponse.getPayment().getActionId()));
-		Assert.assertEquals(paymentRequest.getAmount().intValue(), paymentResponse.getPayment().getAmount());
-		Assert.assertEquals(paymentRequest.getCurrency(), paymentResponse.getPayment().getCurrency());
-		Assert.assertEquals(paymentRequest.getReference(), paymentResponse.getPayment().getReference());
-		Assert.assertNotNull(paymentResponse.getPayment().getDestination());
-		Assert.assertEquals(paymentResponse.getPayment().getDestination().getBin(), paymentRequest.getDestination().getNumber().substring(0, 6));
-		Assert.assertEquals(
-				paymentResponse.getPayment().getDestination().getLast4(),
-				paymentRequest.getDestination().getNumber().substring(paymentRequest.getDestination().getNumber().length() - 4));
-		Assert.assertEquals(paymentRequest.getDestination().getType(), paymentResponse.getPayment().getDestination().getType());
-		Assert.assertEquals(paymentRequest.getDestination().getExpiryMonth(), paymentResponse.getPayment().getDestination().getExpiryMonth());
-		Assert.assertEquals(paymentRequest.getDestination().getExpiryYear(), paymentResponse.getPayment().getDestination().getExpiryYear());
-	}
+        if (paymentResponse.getPending() != null) {
+            final GetPaymentResponse payment = getApi().paymentsClient().getAsync(paymentResponse.getPending().getId()).get();
+            Assert.assertNotNull(payment);
+            Assert.assertFalse(CheckoutUtils.isNullOrEmpty(payment.getId()));
+            Assert.assertEquals(paymentRequest.getAmount(), payment.getAmount());
+            Assert.assertEquals(paymentRequest.getCurrency(), payment.getCurrency());
+            Assert.assertEquals(paymentRequest.getReference(), payment.getReference());
+        } else {
+            Assert.assertNotNull(paymentResponse.getPayment());
+            Assert.assertTrue(paymentResponse.getPayment().isApproved());
+            Assert.assertFalse(CheckoutUtils.isNullOrEmpty(paymentResponse.getPayment().getId()));
+            Assert.assertFalse(CheckoutUtils.isNullOrEmpty(paymentResponse.getPayment().getActionId()));
+            Assert.assertEquals(paymentRequest.getAmount().intValue(), paymentResponse.getPayment().getAmount());
+            Assert.assertEquals(paymentRequest.getCurrency(), paymentResponse.getPayment().getCurrency());
+            Assert.assertEquals(paymentRequest.getReference(), paymentResponse.getPayment().getReference());
+            Assert.assertNotNull(paymentResponse.getPayment().getDestination());
+            Assert.assertEquals(paymentResponse.getPayment().getDestination().getBin(), paymentRequest.getDestination().getNumber().substring(0, 6));
+            Assert.assertEquals(
+                    paymentResponse.getPayment().getDestination().getLast4(),
+                    paymentRequest.getDestination().getNumber().substring(paymentRequest.getDestination().getNumber().length() - 4));
+            Assert.assertEquals(paymentRequest.getDestination().getType(), paymentResponse.getPayment().getDestination().getType());
+            Assert.assertEquals(paymentRequest.getDestination().getExpiryMonth(), paymentResponse.getPayment().getDestination().getExpiryMonth());
+            Assert.assertEquals(paymentRequest.getDestination().getExpiryYear(), paymentResponse.getPayment().getDestination().getExpiryYear());
+        }
+    }
 }
