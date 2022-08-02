57160f66165aa153c4eb04caed7c9329b41545bf
Armando Ibarra <aibarrakerzon@gmail.com>
2022-08-01 20:34:32 -0500

    Added support to ACH and Level 2 & 3 fields Added Palestine Country code

---
+++ src/main/java/com/checkout/payments/response/PassengerName.java
@@ -0,0 +1,18 @@
+package com.checkout.payments.response;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public final class PassengerName {
+
+    @SerializedName("full_name")
+    private String fullName;
+
+}
