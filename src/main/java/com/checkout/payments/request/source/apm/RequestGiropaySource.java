f7974c8782f5bc6f235cb7fb1454763789a4e7f6
Armando Ibarra <aibarrakerzon@gmail.com>
2023-03-01 17:09:40 -0600

    Update GiroPay request object

--- src/main/java/com/checkout/payments/request/source/apm/RequestGiropaySource.java
+++ src/main/java/com/checkout/payments/request/source/apm/RequestGiropaySource.java
@@ -2,48 +2,15 @@ package com.checkout.payments.request.source.apm;
 
 import com.checkout.common.PaymentSourceType;
 import com.checkout.payments.request.source.AbstractRequestSource;
-import com.google.gson.annotations.SerializedName;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
-import java.util.List;
-
-@Getter
-@Setter
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public final class RequestGiropaySource extends AbstractRequestSource {
 
-    private String purpose;
-
-    @SerializedName("info_fields")
-    private List<InfoFields> infoFields;
-
-    @Builder
-    private RequestGiropaySource(final String purpose,
-                                 final List<InfoFields> infoFields) {
-        super(PaymentSourceType.GIROPAY);
-        this.purpose = purpose;
-        this.infoFields = infoFields;
-    }
-
     public RequestGiropaySource() {
         super(PaymentSourceType.GIROPAY);
-    }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class InfoFields {
-        private String label;
-        private String text;
     }
 
 }
