88e0fe0e9af74beb9919f085b5f9d27595072ecb
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2023-08-11 14:11:22 +0200

    Adds Intra values for Region enum and NotSet in CardCategory enum

--- src/main/java/com/checkout/common/CardCategory.java
+++ src/main/java/com/checkout/common/CardCategory.java
@@ -6,11 +6,17 @@ public enum CardCategory {
 
     @SerializedName(value = "Consumer", alternate = {"CONSUMER", "consumer"})
     CONSUMER,
+
     @SerializedName(value = "Commercial", alternate = {"COMMERCIAL", "commercial"})
     COMMERCIAL,
+
     @SerializedName(value = "All", alternate = {"ALL", "all"})
     ALL,
+
     @SerializedName(value = "Other", alternate = {"OTHER", "other"})
-    OTHER
+    OTHER,
+
+    @SerializedName(value = "NotSet", alternate = {"NOTSET", "NOT_SET", "Not_Set", "notset", "not_set"})
+    NOT_SET
 
 }
