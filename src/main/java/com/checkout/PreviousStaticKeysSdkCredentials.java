e13fafaaf00dca63068d024bb1ee398599b9852b
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2023-10-11 10:47:39 +0200

    Fix key patterns to add more compatibility

--- src/main/java/com/checkout/PreviousStaticKeysSdkCredentials.java
+++ src/main/java/com/checkout/PreviousStaticKeysSdkCredentials.java
@@ -2,8 +2,8 @@ package com.checkout;
 
 final class PreviousStaticKeysSdkCredentials extends AbstractStaticKeysSdkCredentials {
 
-    private static final String PREVIOUS_SECRET_KEY_PATTERN = "^sk_(((test_)?(\\w{8})-(\\w{4})-(\\w{4})-(\\w{4})-(\\w{12}))|((sbox_)?[a-z2-7]{26}[a-z2-7*#$=]))$";
-    private static final String PREVIOUS_PUBLIC_KEY_PATTEN = "^pk_(((test_)?(\\w{8})-(\\w{4})-(\\w{4})-(\\w{4})-(\\w{12}))|((sbox_)?[a-z2-7]{26}[a-z2-7*#$=]))$";
+    private static final String PREVIOUS_SECRET_KEY_PATTERN = "^(Bearer )?sk_(((test_)?(\\w{8})-(\\w{4})-(\\w{4})-(\\w{4})-(\\w{12}))|((sbox_)?[a-z2-7]{26}[a-z2-7*#$=]))$";
+    private static final String PREVIOUS_PUBLIC_KEY_PATTEN = "^(Bearer )?pk_(((test_)?(\\w{8})-(\\w{4})-(\\w{4})-(\\w{4})-(\\w{12}))|((sbox_)?[a-z2-7]{26}[a-z2-7*#$=]))$";
 
     PreviousStaticKeysSdkCredentials(final String secretKey, final String publicKey) {
         super(PlatformType.PREVIOUS, PREVIOUS_SECRET_KEY_PATTERN, PREVIOUS_PUBLIC_KEY_PATTEN, secretKey, publicKey);
