8de249462678b1cd6e106f9026a369f801d152da
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-04-05 11:45:12 +0200

    Update purpose types

--- src/main/java/com/checkout/accounts/AccountsFilePurpose.java
+++ src/main/java/com/checkout/accounts/AccountsFilePurpose.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 public enum AccountsFilePurpose {
 
     BANK_VERIFICATION("bank_verification"),
+    IDENTIFICATION("identification"),
     IDENTITY_VERIFICATION("identity_verification"),
     COMPANY_VERIFICATION("company_verification"),
-    FINANCIAL_VERIFICATION("financial_verification");
+    FINANCIAL_VERIFICATION("financial_verification"),
+    TAX_VERIFICATION("tax_verification");
 
     @Getter
     private final String purpose;
