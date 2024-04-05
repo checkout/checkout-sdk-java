36cd9186d01f026d28db91e8386b16a6bbc20543
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-04-05 11:40:52 +0200

    Update card metadata response properties

--- src/main/java/com/checkout/common/SchemeLocalType.java
+++ src/main/java/com/checkout/common/SchemeLocalType.java
@@ -4,10 +4,20 @@ import com.google.gson.annotations.SerializedName;
 
 public enum SchemeLocalType {
 
+    @SerializedName("accel")
+    ACCEL,
     @SerializedName("cartes_bancaires")
     CARTES_BANCAIRES,
     @SerializedName("mada")
     MADA,
+    @SerializedName("nyce")
+    NYCE,
     @SerializedName("omannet")
     OMANNET,
+    @SerializedName("pulse")
+    PULSE,
+    @SerializedName("star")
+    STAR,
+    @SerializedName("upi")
+    UPI,
 }
