0b9930a8d4021839542904d10280e348e537d606
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-07-30 18:50:20 +0200

    Add partial authorization in payment request

---
+++ src/main/java/com/checkout/payments/request/PartialAuthorization.java
@@ -0,0 +1,16 @@
+package com.checkout.payments.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public final class PartialAuthorization {
+
+    private Boolean enabled;
+
+}
