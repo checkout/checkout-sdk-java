c41404daf0905a60e8c4bb2def3694726af4d6d7
david ruiz <david.ruiz@checkout.com>
2025-12-29 16:32:11 +0100

    SessionsClient sync methods + tests

--- src/main/java/com/checkout/sessions/SessionsClientImpl.java
+++ src/main/java/com/checkout/sessions/SessionsClientImpl.java
@@ -37,57 +37,8 @@ public class SessionsClientImpl extends AbstractClient implements SessionsClient
 
     @Override
     public CompletableFuture<SessionResponse> requestSession(final SessionRequest sessionRequest) {
-        validateParams("sessionRequest", sessionRequest);
-        return createSession(sessionRequest);
-    }
-
-    @Override
-    public CompletableFuture<GetSessionResponse> getSessionDetails(final String sessionId) {
-        return getSessionDetails(sessionId, sdkAuthorization());
-    }
-
-    @Override
-    public CompletableFuture<GetSessionResponse> getSessionDetails(final String sessionSecret, final String sessionId) {
-        return getSessionDetails(sessionId, sessionSecretAuthorization(sessionSecret));
-    }
-
-    @Override
-    public final CompletableFuture<GetSessionResponse> updateSession(final String sessionId, final ChannelData channelData) {
-        return updateSession(sessionId, channelData, sdkAuthorization());
-    }
-
-    @Override
-    public CompletableFuture<GetSessionResponse> updateSession(final String sessionSecret,
-                                                               final String sessionId,
-                                                               final ChannelData channelData) {
-        return updateSession(sessionId, channelData, sessionSecretAuthorization(sessionSecret));
-    }
-
-    @Override
-    public CompletableFuture<EmptyResponse> completeSession(final String sessionId) {
-        return completeSession(sessionId, sdkAuthorization());
-    }
-
-    @Override
-    public CompletableFuture<EmptyResponse> completeSession(final String sessionSecret, final String sessionId) {
-        return completeSession(sessionId, sessionSecretAuthorization(sessionSecret));
-    }
-
-    @Override
-    public CompletableFuture<GetSessionResponseAfterChannelDataSupplied> update3dsMethodCompletionIndicator(final String sessionId,
-                                                                                                            final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest) {
-        return update3dsMethodCompletionIndicator(sessionId, threeDsMethodCompletionRequest, sdkAuthorization());
-    }
-
-    @Override
-    public CompletableFuture<GetSessionResponseAfterChannelDataSupplied> update3dsMethodCompletionIndicator(final String sessionSecret,
-                                                                                                            final String sessionId,
-                                                                                                            final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest) {
-        return update3dsMethodCompletionIndicator(sessionId, threeDsMethodCompletionRequest, sessionSecretAuthorization(sessionSecret));
-    }
-
-    private CompletableFuture<SessionResponse> createSession(final SessionRequest sessionRequest) {
-        return apiClient.postAsync(SESSIONS_PATH, sdkAuthorization(), SESSION_RESPONSE_MAPPINGS, sessionRequest, null)
+        validateSessionRequest(sessionRequest);
+        return apiClient.postAsync(SESSIONS_PATH, resolveAuthorization(), SESSION_RESPONSE_MAPPINGS, sessionRequest, null)
                 .thenApply((Function<HttpMetadata, SessionResponse>) resource -> {
                     if (resource instanceof CreateSessionOkResponse) {
                         return new SessionResponse((CreateSessionOkResponse) resource);
@@ -99,33 +50,145 @@ public class SessionsClientImpl extends AbstractClient implements SessionsClient
                 });
     }
 
-    private CompletableFuture<GetSessionResponse> getSessionDetails(final String sessionId,
-                                                                    final SdkAuthorization sdkAuthorization) {
-        validateParams(SESSION_ID, sessionId);
-        return apiClient.getAsync(buildPath(SESSIONS_PATH, sessionId), sdkAuthorization, GetSessionResponse.class);
+    @Override
+    public CompletableFuture<GetSessionResponse> getSessionDetails(final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.getAsync(buildPath(SESSIONS_PATH, sessionId), resolveAuthorization(), GetSessionResponse.class);
     }
 
-    private CompletableFuture<GetSessionResponse> updateSession(final String sessionId,
-                                                                final ChannelData channelData,
-                                                                final SdkAuthorization sdkAuthorization) {
+    @Override
+    public CompletableFuture<GetSessionResponse> getSessionDetails(final String sessionSecret, final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.getAsync(buildPath(SESSIONS_PATH, sessionId), resolveAuthorization(sessionSecret), GetSessionResponse.class);
+    }
+
+    @Override
+    public final CompletableFuture<GetSessionResponse> updateSession(final String sessionId, final ChannelData channelData) {
+        validateSessionIdAndChannelData(sessionId, channelData);
+        return apiClient.putAsync(buildPath(SESSIONS_PATH, sessionId, COLLECT_DATA_PATH), resolveAuthorization(), GetSessionResponse.class, channelData);
+    }
+
+    @Override
+    public CompletableFuture<GetSessionResponse> updateSession(final String sessionSecret,
+                                                               final String sessionId,
+                                                               final ChannelData channelData) {
+        validateSessionIdAndChannelData(sessionId, channelData);
+        return apiClient.putAsync(buildPath(SESSIONS_PATH, sessionId, COLLECT_DATA_PATH), resolveAuthorization(sessionSecret), GetSessionResponse.class, channelData);
+    }
+
+    @Override
+    public CompletableFuture<EmptyResponse> completeSession(final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.postAsync(buildPath(SESSIONS_PATH, sessionId, COMPLETE_PATH), resolveAuthorization(), EmptyResponse.class, null, null);
+    }
+
+    @Override
+    public CompletableFuture<EmptyResponse> completeSession(final String sessionSecret, final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.postAsync(buildPath(SESSIONS_PATH, sessionId, COMPLETE_PATH), resolveAuthorization(sessionSecret), EmptyResponse.class, null, null);
+    }
+
+    @Override
+    public CompletableFuture<GetSessionResponseAfterChannelDataSupplied> update3dsMethodCompletionIndicator(final String sessionId,
+                                                                                                            final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest) {
+        validateSessionIdAndThreeDsRequest(sessionId, threeDsMethodCompletionRequest);
+        return apiClient.putAsync(buildPath(SESSIONS_PATH, sessionId, ISSUER_FINGERPRINT_PATH), resolveAuthorization(), GetSessionResponseAfterChannelDataSupplied.class, threeDsMethodCompletionRequest);
+    }
+
+    @Override
+    public CompletableFuture<GetSessionResponseAfterChannelDataSupplied> update3dsMethodCompletionIndicator(final String sessionSecret,
+                                                                                                            final String sessionId,
+                                                                                                            final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest) {
+        validateSessionIdAndThreeDsRequest(sessionId, threeDsMethodCompletionRequest);
+        return apiClient.putAsync(buildPath(SESSIONS_PATH, sessionId, ISSUER_FINGERPRINT_PATH), resolveAuthorization(sessionSecret), GetSessionResponseAfterChannelDataSupplied.class, threeDsMethodCompletionRequest);
+    }
+
+    // Synchronous methods
+    @Override
+    public SessionResponse requestSessionSync(final SessionRequest sessionRequest) {
+        validateSessionRequest(sessionRequest);
+        final HttpMetadata resource = apiClient.post(SESSIONS_PATH, resolveAuthorization(), SESSION_RESPONSE_MAPPINGS, sessionRequest, null);
+        if (resource instanceof CreateSessionOkResponse) {
+            return new SessionResponse((CreateSessionOkResponse) resource);
+        } else if (resource instanceof CreateSessionAcceptedResponse) {
+            return new SessionResponse((CreateSessionAcceptedResponse) resource);
+        } else {
+            throw new IllegalStateException("Unexpected mapping type " + resource.getClass());
+        }
+    }
+
+    @Override
+    public GetSessionResponse getSessionDetailsSync(final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.get(buildPath(SESSIONS_PATH, sessionId), resolveAuthorization(), GetSessionResponse.class);
+    }
+
+    @Override
+    public GetSessionResponse getSessionDetailsSync(final String sessionSecret, final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.get(buildPath(SESSIONS_PATH, sessionId), resolveAuthorization(sessionSecret), GetSessionResponse.class);
+    }
+
+    @Override
+    public GetSessionResponse updateSessionSync(final String sessionId, final ChannelData channelData) {
+        validateSessionIdAndChannelData(sessionId, channelData);
+        return apiClient.put(buildPath(SESSIONS_PATH, sessionId, COLLECT_DATA_PATH), resolveAuthorization(), GetSessionResponse.class, channelData);
+    }
+
+    @Override
+    public GetSessionResponse updateSessionSync(final String sessionSecret, final String sessionId, final ChannelData channelData) {
+        validateSessionIdAndChannelData(sessionId, channelData);
+        return apiClient.put(buildPath(SESSIONS_PATH, sessionId, COLLECT_DATA_PATH), resolveAuthorization(sessionSecret), GetSessionResponse.class, channelData);
+    }
+
+    @Override
+    public EmptyResponse completeSessionSync(final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.post(buildPath(SESSIONS_PATH, sessionId, COMPLETE_PATH), resolveAuthorization(), EmptyResponse.class, null, null);
+    }
+
+    @Override
+    public EmptyResponse completeSessionSync(final String sessionSecret, final String sessionId) {
+        validateSessionId(sessionId);
+        return apiClient.post(buildPath(SESSIONS_PATH, sessionId, COMPLETE_PATH), resolveAuthorization(sessionSecret), EmptyResponse.class, null, null);
+    }
+
+    @Override
+    public GetSessionResponseAfterChannelDataSupplied update3dsMethodCompletionIndicatorSync(final String sessionId, final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest) {
+        validateSessionIdAndThreeDsRequest(sessionId, threeDsMethodCompletionRequest);
+        return apiClient.put(buildPath(SESSIONS_PATH, sessionId, ISSUER_FINGERPRINT_PATH), resolveAuthorization(), GetSessionResponseAfterChannelDataSupplied.class, threeDsMethodCompletionRequest);
+    }
+
+    @Override
+    public GetSessionResponseAfterChannelDataSupplied update3dsMethodCompletionIndicatorSync(final String sessionSecret, final String sessionId, final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest) {
+        validateSessionIdAndThreeDsRequest(sessionId, threeDsMethodCompletionRequest);
+        return apiClient.put(buildPath(SESSIONS_PATH, sessionId, ISSUER_FINGERPRINT_PATH), resolveAuthorization(sessionSecret), GetSessionResponseAfterChannelDataSupplied.class, threeDsMethodCompletionRequest);
+    }
+
+    // Common methods
+    protected void validateSessionRequest(final SessionRequest sessionRequest) {
+        validateParams("sessionRequest", sessionRequest);
+    }
+
+    protected void validateSessionId(final String sessionId) {
+        validateParams(SESSION_ID, sessionId);
+    }
+
+    protected void validateSessionIdAndChannelData(final String sessionId, final ChannelData channelData) {
         validateParams(SESSION_ID, sessionId, "channelData", channelData);
-        return apiClient.putAsync(buildPath(SESSIONS_PATH, sessionId, COLLECT_DATA_PATH), sdkAuthorization, GetSessionResponse.class, channelData);
     }
 
-    private CompletableFuture<EmptyResponse> completeSession(final String sessionId,
-                                                             final SdkAuthorization sdkAuthorization) {
-        validateParams(SESSION_ID, sessionId);
-        return apiClient.postAsync(buildPath(SESSIONS_PATH, sessionId, COMPLETE_PATH), sdkAuthorization, EmptyResponse.class, null, null);
-    }
 
-    private CompletableFuture<GetSessionResponseAfterChannelDataSupplied> update3dsMethodCompletionIndicator(final String sessionId,
-                                                                                                             final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest,
-                                                                                                             final SdkAuthorization sdkAuthorization) {
+    protected void validateSessionIdAndThreeDsRequest(final String sessionId, final ThreeDsMethodCompletionRequest threeDsMethodCompletionRequest) {
         validateParams(SESSION_ID, sessionId, "threeDsMethodCompletionRequest", threeDsMethodCompletionRequest);
-        return apiClient.putAsync(buildPath(SESSIONS_PATH, sessionId, ISSUER_FINGERPRINT_PATH), sdkAuthorization, GetSessionResponseAfterChannelDataSupplied.class, threeDsMethodCompletionRequest);
     }
 
-    private SdkAuthorization sessionSecretAuthorization(final String sessionSecret) {
+    // Authorization resolver methods
+    private SdkAuthorization resolveAuthorization() {
+        return sdkAuthorization();
+    }
+
+    private SdkAuthorization resolveAuthorization(final String sessionSecret) {
         validateParams("sessionSecret", sessionSecret);
         return new SessionSecretSdkCredentials(sessionSecret).getAuthorization(SdkAuthorizationType.CUSTOM);
     }
