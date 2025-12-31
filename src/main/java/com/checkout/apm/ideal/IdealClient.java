80bbac1317d05c0df4ef46c0ea67fd133f702c44
david ruiz <david.ruiz@checkout.com>
2025-12-31 17:54:11 +0100

    IdealClient sync methods + tests

--- src/main/java/com/checkout/apm/ideal/IdealClient.java
+++ src/main/java/com/checkout/apm/ideal/IdealClient.java
@@ -5,7 +5,11 @@ import java.util.concurrent.CompletableFuture;
 public interface IdealClient {
 
     CompletableFuture<IdealInfo> getInfo();
-
+    
     CompletableFuture<IssuerResponse> getIssuers();
 
+    // Synchronous methods
+    IdealInfo getInfoSync();
+
+    IssuerResponse getIssuersSync();
 }
