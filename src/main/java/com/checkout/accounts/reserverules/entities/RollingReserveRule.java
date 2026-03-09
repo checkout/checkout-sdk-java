2ae6c94303cb2141c48eaabf55a3067c4b0ef9a3
david ruiz <david.ruiz@checkout.com>
2026-03-09 16:16:11 +0100

    RollingReserveRule rename

--- src/main/java/com/checkout/accounts/reserverules/entities/RollingReserveRule.java
+++ src/main/java/com/checkout/accounts/reserverules/entities/RollingReserveRule.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class RollingReservePolicy {
+public class RollingReserveRule {
     private double percentage;
 
     @SerializedName("holding_duration")
--- src/main/java/com/checkout/accounts/reserverules/responses/ReserveRuleRequest.java
+++ src/main/java/com/checkout/accounts/reserverules/responses/ReserveRuleRequest.java
@@ -1,7 +1,7 @@
 package com.checkout.accounts.reserverules.responses;
 
 import com.checkout.accounts.Headers;
-import com.checkout.accounts.reserverules.entities.RollingReservePolicy;
+import com.checkout.accounts.reserverules.entities.RollingReserveRule;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,7 +21,7 @@ public class ReserveRuleRequest extends Headers {
 
     private String type;
 
-    private RollingReservePolicy rolling;
+    private RollingReserveRule rolling;
 
     private Instant validFrom;
 
