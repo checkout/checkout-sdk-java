89c28d912a45ac9d4dcb49446534e82a1c7f5e71
david ruiz <david.ruiz@checkout.com>
2026-03-12 12:12:59 +0100

    StandaloneAccountUpdaterClient initial development (must review)

---
+++ src/main/java/com/checkout/standaloneaccountupdater/responses/GetUpdatedCardCredentialsResponse.java
@@ -0,0 +1,30 @@
+package com.checkout.standaloneaccountupdater.responses;
+
+import com.checkout.common.Resource;
+import com.checkout.standaloneaccountupdater.entities.AccountUpdateStatus;
+import com.checkout.standaloneaccountupdater.entities.AccountUpdateFailureCode;
+import com.checkout.standaloneaccountupdater.entities.CardUpdated;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class GetUpdatedCardCredentialsResponse extends Resource {
+
+    /**
+     * Result of the update operation.
+     * [Required]
+     */
+    private AccountUpdateStatus accountUpdateStatus;
+
+    /**
+     * This field is returned when the update fails and the scheme returns an appropriate reason code.
+     * For more information, see Standalone Account Updater
+     */
+    private AccountUpdateFailureCode accountUpdateFailureCode;
+
+    /**
+     * Updated card details. Fields vary depending on PCI compliance level.
+     */
+    private CardUpdated card;
+}
