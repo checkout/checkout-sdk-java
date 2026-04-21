8bb78261f543762ff75e12e60d826885e057dd5f
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2026-04-21 18:24:38 +0200

    feat(issuing): update card shipping instruction fields
    Align ShippingInstruction with current swagger schema for issuing cards.

--- src/main/java/com/checkout/issuing/cards/requests/create/ShippingInstruction.java
+++ src/main/java/com/checkout/issuing/cards/requests/create/ShippingInstruction.java
@@ -9,12 +9,28 @@ import lombok.Data;
 @Builder
 public final class ShippingInstruction {
 
+    /**
+     * The name of the person the card will be shipped to.
+     * [Optional]
+     * @deprecated No longer supported.
+     */
+    @Deprecated
     @SerializedName("shipping_recipient")
     private String shippingRecipient;
 
+    /**
+     * The address to ship the physical card to.
+     * [Optional]
+     */
     @SerializedName("shipping_address")
     private Address shippingAddress;
 
+    /**
+     * Any additional comment on shipping.
+     * [Optional]
+     * @deprecated No longer supported.
+     */
+    @Deprecated
     @SerializedName("additional_comment")
     private String additionalComment;
 }
