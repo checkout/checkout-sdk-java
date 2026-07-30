0ac5d6c8419da5b9d29f0d5b05e29fc98f44b9bd
david ruiz <david.ruiz@checkout.com>
2026-07-30 12:18:07 +0200

    CardCategory and CardType fixes

--- src/main/java/com/checkout/common/CardCategory.java
+++ src/main/java/com/checkout/common/CardCategory.java
@@ -4,15 +4,11 @@ import com.google.gson.annotations.SerializedName;
 
 public enum CardCategory {
 
-    @SerializedName(value = "All", alternate = {"ALL", "all"})
-    ALL,
     @SerializedName(value = "Commercial", alternate = {"COMMERCIAL", "commercial"})
     COMMERCIAL,
     @SerializedName(value = "Consumer", alternate = {"CONSUMER", "consumer"})
     CONSUMER,
-    @SerializedName(value = "NotSet", alternate = {"NOTSET", "NOT_SET", "Not_Set", "notset", "not_set"})
-    NOT_SET,
-    @SerializedName(value = "Other", alternate = {"OTHER", "other"})
-    OTHER
+    @SerializedName(value = "Unknown", alternate = {"UNKNOWN", "unknown"})
+    UNKNOWN
 
 }
--- src/main/java/com/checkout/common/CardType.java
+++ src/main/java/com/checkout/common/CardType.java
@@ -12,7 +12,14 @@ public enum CardType {
     PREPAID,
     @SerializedName(value = "Charge", alternate = {"CHARGE", "charge"})
     CHARGE,
-    @SerializedName(value = "Deferred Debit", alternate = {"DEFERRED DEBIT", "deferred_debit"})
+    @SerializedName(value = "Deferred Debit", alternate = {"DEFERRED DEBIT", "deferred debit"})
     DEFERRED_DEBIT,
+
+    // NetworkToken is returned only by the card metadata endpoint.
+    @SerializedName(value = "Network Token", alternate = {"NETWORK TOKEN", "network token"})
+    NETWORK_TOKEN,
+    // Unknown is returned only on card payout destinations.
+    @SerializedName(value = "Unknown", alternate = {"UNKNOWN", "unknown"})
+    UNKNOWN
 
 }
